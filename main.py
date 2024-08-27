2037ce04b0b4928be3b3f433ca52801b7977ff2d
Drew Elliott <drew@beaverlab.dev>
2024-08-27 12:00:29 -0400

    Player ship working

--- main.py
+++ main.py
@@ -3,19 +3,40 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+
+    Player.containers = (updatable, drawable)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
+
+
 
 
 if __name__ == "__main__":
