37c1379d90756d14b8d15a22bab069909dc99e42
Drew Elliott <drew@beaverlab.dev>
2024-09-05 11:53:58 -0400

    splitting asteroids if shot

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
             for shot in shots:
                 if asteroid.collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
